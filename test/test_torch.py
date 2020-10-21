8db05199647de6bed479db41b4522bb358e79a6d
anjali411 <chourdiaanjali123@gmail.com>
2020-10-21 07:49:14 -0700

    Add complex support for torch.addcmul and torch.addcdiv
    ghstack-source-id: afb894265f1450cb80691bdd9fa72cf4218449e8
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/46639

--- test/test_torch.py
+++ test/test_torch.py
@@ -12513,10 +12513,6 @@ class TestTorchDeviceType(TestCase):
             else:
                 alpha = 3
 
-            # addcmul is not supported for complex dtypes on cuda yet
-            if device.startswith('cuda') and dtype.is_complex:
-                continue
-
             actual = torch.addcmul(a, b, c, value=alpha)
             expected = a + alpha * b * c
 
@@ -13436,7 +13432,7 @@ class TestTorchDeviceType(TestCase):
         def _test_addcdiv(a, alpha, b, c):
             actual = torch.addcdiv(a, b, c, value=alpha)
             # implementation of addcdiv downcasts alpha. arithmetic ops don't.
-            if not actual.dtype.is_floating_point:
+            if not (actual.dtype.is_floating_point or actual.dtype.is_complex):
                 alpha = int(alpha)
             expected = a + (alpha * b) / c
             self.assertEqual(expected, actual)
@@ -13462,17 +13458,7 @@ class TestTorchDeviceType(TestCase):
                 non_zero_rand((2, 2), dtype=dtype, device=device))
 
         for dtype in torch.testing.get_all_math_dtypes(device):
-            if dtype.is_complex:
-                # CPU complex addcdiv is wildly inaccurate
-                if self.device_type == 'cpu':
-                    with self.assertRaises(AssertionError):
-                        _helper()
-
-                # CUDA complex addcdiv is not implemented
-                if self.device_type == 'cuda':
-                    with self.assertRaises(RuntimeError):
-                        _helper()
-            elif not dtype.is_floating_point:
+            if not (dtype.is_floating_point or dtype.is_complex):
                 # Integer division with addcdiv is prohibited
                 with self.assertRaises(RuntimeError):
                     _helper()
